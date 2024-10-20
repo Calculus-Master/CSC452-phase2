653ff4144f9501b73b0eb082700859953f04d9d2
Saptarshi Mallick <sapmallick@gmail.com>
2024-10-19 21:22:56 -0700

    Added MboxSend

--- phase2.c
+++ phase2.c
@@ -7,13 +7,18 @@
 // Data Structures
 typedef struct ShadowProcess {
     int pid;
+
     struct ShadowProcess* consumer_queue_next;
     struct ShadowProcess* producer_queue_next;
 } ShadowProcess;
 
 typedef struct MailSlot {
     int mailbox_id;
+    int claimed_by_pid;
+
+    int message_size;
     char message[MAX_MESSAGE];
+
     struct MailSlot* queue_next;
 } MailSlot;
 
@@ -21,7 +26,8 @@ typedef struct MailBox {
     short in_use;
     short flagged_for_removal;
 
-    int num_slots;
+    int used_slots;
+    int max_slots;
     int slot_size;
 
     MailSlot* slot_queue;
@@ -45,6 +51,22 @@ void check_kernel_mode(const char *function_name)
     }
 }
 
+int is_valid_mailbox_id(int mbox_id)
+{
+    if(mbox_id < 0 || mbox_id >= MAXMBOX)
+        return 0;
+
+    return mailboxes[mbox_id].in_use;
+}
+
+int get_next_free_mailbox_slot()
+{
+    for(int i = 0; i < MAXSLOTS; i++)
+        if(mailslots[i].mailbox_id == -1)
+            return i;
+    return -1;
+}
+
 // Phase 2 Functions
 
 void phase2_init(void)
@@ -53,6 +75,20 @@ void phase2_init(void)
     memset(mailboxes, 0, sizeof(mailboxes));
     memset(mailslots, 0, sizeof(mailslots));
     memset(shadow_table, 0, sizeof(shadow_table));
+
+    // Set mailslot mailbox_id to -1 to indicate it's free
+    for(int i = 0; i < MAXSLOTS; i++)
+        mailslots[i].mailbox_id = -1;
+
+    // Create device mailboxes
+    // TODO idk how these get used, but they probably need to be stored somewhere
+    int clock1_mbox = MboxCreate(1, sizeof(int));
+    int disk1_mbox = MboxCreate(1, sizeof(int));
+    int disk2_mbox = MboxCreate(1, sizeof(int));
+    int term1_mbox = MboxCreate(1, sizeof(int));
+    int term2_mbox = MboxCreate(1, sizeof(int));
+    int term3_mbox = MboxCreate(1, sizeof(int));
+    int term4_mbox = MboxCreate(1, sizeof(int));
 }
 
 // returns id of mailbox, or -1 if no more mailboxes, or -1 if invalid args
@@ -79,7 +115,7 @@ int MboxCreate(int slots, int slot_size)
 
     // Mailbox initialization
     mbox->in_use = 1;
-    mbox->num_slots = slots;
+    mbox->max_slots = slots;
     mbox->slot_size = slot_size;
 
     return next_mbox_id - 1;
@@ -91,12 +127,8 @@ int MboxRelease(int mbox_id)
     check_kernel_mode(__func__ );
 
     // Check if mbox_id is valid
-    int valid = 0;
-    for(int i = 0; i < MAXMBOX; i++)
-        if(mailboxes[i].in_use)
-            valid = 1;
-
-    if(!valid) return -1;
+    if(!is_valid_mailbox_id(mbox_id))
+        return -1;
 
     MailBox* mailbox = &mailboxes[mbox_id];
 
@@ -109,6 +141,7 @@ int MboxRelease(int mbox_id)
     {
         MailSlot* next = slot->queue_next;
         memset(slot, 0, sizeof(MailSlot));
+        slot->mailbox_id = -1;
         slot = next;
     }
 
@@ -138,6 +171,80 @@ int MboxRelease(int mbox_id)
 int MboxSend(int mbox_id, void *msg_ptr, int msg_size)
 {
     check_kernel_mode(__func__ );
+
+    // Check if mbox_id is valid
+    if(!is_valid_mailbox_id(mbox_id))
+        return -1;
+
+    MailBox* mailbox = &mailboxes[mbox_id];
+
+    if(mailbox->flagged_for_removal)
+        return -1; // Mailbox is flagged for removal
+    else if(mailbox->used_slots == mailbox->max_slots) // Mailbox is full, add process to producer queue
+    {
+        ShadowProcess* process = &shadow_table[getpid() % MAXPROC];
+
+        // Add process to producer queue
+        if(mailbox->producer_queue == NULL)
+            mailbox->producer_queue = process;
+        else
+        {
+            ShadowProcess* last_process = mailbox->producer_queue;
+            while(last_process->producer_queue_next != NULL)
+                last_process = last_process->producer_queue_next;
+            last_process->producer_queue_next = process;
+        }
+
+        // Block (when the mailbox has space it'll unblock)
+        blockMe();
+    }
+
+    int slot_index = get_next_free_mailbox_slot();
+
+    if(slot_index == -1)
+        return -2; // No global free slots
+
+    // Copy message into slot
+    MailSlot* slot = &mailslots[slot_index];
+    slot->mailbox_id = mbox_id;
+    memcpy(slot->message, msg_ptr, msg_size);
+    slot->message_size = msg_size;
+
+    // Add slot to mailbox
+    MailSlot* last_slot = mailbox->slot_queue;
+    if(last_slot == NULL)
+        mailbox->slot_queue = slot;
+    else
+    {
+        while(last_slot->queue_next != NULL)
+            last_slot = last_slot->queue_next;
+        last_slot->queue_next = slot;
+    }
+
+    // If no consumers, block
+    if(mailbox->consumer_queue == NULL)
+        blockMe();
+    // Otherwise, wake up a consumer and deliver
+    // TODO: PROBABLY BUGGY
+    else
+    {
+        // Remove first consumer from queue
+        ShadowProcess* consumer = mailbox->consumer_queue;
+        mailbox->consumer_queue = consumer->consumer_queue_next;
+
+        // Set the first unclaimed slot in the queue to be claimed by the first consumer
+        // Does not remove the slot from the queue, that is done in MboxRecv()
+        // Hopefully avoids the race condition
+        MailSlot* deliver_slot = mailbox->slot_queue;
+        while(deliver_slot->claimed_by_pid)
+            deliver_slot = deliver_slot->queue_next;
+        deliver_slot->claimed_by_pid = consumer->pid;
+
+        // Unblock consumer, it will eventually grab this message
+        unblockProc(consumer->pid);
+    }
+
+    return 0;
 }
 
 // returns size of received msg if successful, -1 if invalid args
